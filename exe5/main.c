8736b1f6c8484c89ae57a011ec0f9a1a3c8d70d5
jlbrasileiro <lucas_jl07@hotmail.com>
2026-02-20 14:12:39 -0300

    ex5 test

--- exe5/main.c
+++ exe5/main.c
@@ -21,11 +21,24 @@ int main() {
     while (true) {
 
         if (!gpio_get(BTN_PIN)) {
+            sleep_ms(50); 
+            
             printf("Botao 1: %d\n", cnt_1++);
+            
+            while (!gpio_get(BTN_PIN)) {
+                sleep_ms(10);
+            }
         }
 
         if (!gpio_get(BTN_PIN_2)) {
+            sleep_ms(50); 
             printf("Botao 2: %d\n", cnt_2++);
+            
+            while (!gpio_get(BTN_PIN_2)) {
+                sleep_ms(10);
+            }
         }
+        
+        sleep_ms(10);
     }
 }
