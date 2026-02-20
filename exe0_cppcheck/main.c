ec9322c18ece6a631299c75d2a3dff1620fc740d
jlbrasileiro <lucas_jl07@hotmail.com>
2026-02-20 09:35:04 -0300

    ex0 teste 2

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -13,9 +13,12 @@ int main(void) {
     int b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
 
+    printf("O resultado da multiplicacao eh: %d\n", result);
+
     int arr[NUM_ELEMENTS];
     for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
+        printf("arr[%d] = %d\n", i, arr[i]);
     }
 
     return 0;
