b1ab6c803ee8b53095b5633cf18d3d19d2bd2630
jlbrasileiro <lucas_jl07@hotmail.com>
2026-02-20 14:27:05 -0300

    ex6 test2

--- exe6/main.c
+++ exe6/main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 
-int FIRST_GPIO = 2;
+const int FIRST_GPIO = 2;
 const int BTN_PIN_G = 28;
 
-int BUTTON_GPIO;
-int cnt;
-int last_btn; // Button not pressed (pulled up)
-
-// This array converts a number 0-9 to a bit pattern to send to the GPIOs
-int bits[10] = {
+const int bits[10] = {
     0x3f,  // 0
     0x06,  // 1
     0x5b,  // 2
@@ -31,8 +25,8 @@ void seven_seg_init() {
     }
 }
 
-void seven_seg_display() {
-    int value = bits[cnt];
+void seven_seg_display(int val) {
+    int value = bits[val];
     for (int i = 0; i < 7; i++) {
         int gpio = FIRST_GPIO + i;
         int bit = (value >> i) & 1;
@@ -42,26 +36,28 @@ void seven_seg_display() {
 
 int main() {
     stdio_init_all();
-    int aux = 0;
-
-    BUTTON_GPIO = FIRST_GPIO + 7;
+    
+    int cnt = 0;
+    int last_btn = 1;
 
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
 
     seven_seg_init();
-    seven_seg_display(2);
+    seven_seg_display(cnt);
 
     while (true) {
         int btn = gpio_get(BTN_PIN_G);
+        
         if (last_btn && !btn) { 
             if (++cnt > 9) {
                 cnt = 0;
             }
-            seven_seg_display();
-            printf("cnt: %l\n", cnt);
+            seven_seg_display(cnt);
+            printf("cnt: %d\n", cnt); 
         }
+        
         last_btn = btn;
         sleep_ms(10); 
     }
